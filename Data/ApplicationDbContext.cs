d2b1ff2daa5962a9154b1f8717de4a622404fbbf
Jasmine Youngharry <jazzyyoungharr@gmail.com>
2026-05-06 15:16:50 -0600

    Add ApplicationDbContext for database access

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
