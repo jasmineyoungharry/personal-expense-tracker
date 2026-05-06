9792375f4a9a7d8862609ae848bea740e93b8ba5
Jasmine Youngharry <jazzyyoungharr@gmail.com>
2026-05-06 17:21:33 -0600

    Add ExpensesController and Index view manually

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using System.Linq;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpensesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Expenses
+        public IActionResult Index()
+        {
+            var expenses = _context.Expenses.ToList();
+            return View(expenses);
+        }
+
+    }
+}
