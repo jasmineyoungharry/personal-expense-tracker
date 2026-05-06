1e9a15b9737354bcd94930acb7894876b05cfe36
Jasmine Youngharry <jazzyyoungharr@gmail.com>
2026-05-06 15:12:15 -0600

    Add Expense model

---
+++ Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Category { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
